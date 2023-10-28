a4747b766ef79b106908af853d20ea133134f636
Aryan Siddiqui <aryansid@stanford.edum>
2023-10-28 14:21:26 -0700

    User command + voice output + prompt engineering

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+@app.route('/audio/<filename>', methods=['GET'])
+def serve_audio(filename):
+    return send_from_directory(os.path.join('./audio_files'), filename, mimetype='audio/wav')
 
 @app.route('/process', methods=['POST'])
 def process(): 
   try: 
-    # image_url = request.form['image_url']
-    
+        
     print("Received request to process image.")
     file = request.files['file']
+    question = request.form['question']
+    
     if file and allowed_file(file.filename):
       filename = secure_filename(file.filename)
       filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -31,24 +36,27 @@ def process():
       dense_captions = ml.get_dense_captions(filepath)
       print("Received dense captions from Azure.")
     
-      PROMPT = """You are a precise narrator assisting a blind individual in real-time. Your task is to create a brief, yet spatially organized and immersize description of their surroundings based on the data provided. Start by describing what's near them and then systematically move to distant objects. Prioritize and include only essential details that signficantly contribute to an immersive experience. Aim for brevity but don't sacrifice the essence of the experience."""
+      PROMPT = "You are tasked with answering a blind individual's question based on dense captions that describe their current environment. Use these captions to construct a spatially organized and immersive answer. Prioritize objects with the highest confidence scores. Aim for brevity without sacrificing the experience."
+      # PROMPT = "You are a sophisticated AI guide who translates the visually descriptive dense captions into precise and straightforward answers to a blind individual's questions. Your task is to weave these dense captions into a concise yet informative response, focusing on objects with higher confidence scores or those most relevant to the question. The goal is to make the experience as seamless and immersive as possible for the blind user."
+      if question: 
+        PROMPT += f"\n\nUser's Question: {question}"
       gpt_response = ml.call_gpt_model(PROMPT, json.dumps(dense_captions, indent=4), "gpt-3.5-turbo")
       print("GPT-3 response received.")
       
-      audio_directory = './audio_files'  # Choose a directory to save audio files
+      audio_directory = './audio_files'  
       if not os.path.exists(audio_directory):
           os.makedirs(audio_directory)
 
       audio_filename = os.path.join(audio_directory, f"audio_{uuid.uuid4()}.wav")
       ml.text_to_audio(gpt_response, audio_filename, os.getenv("tts_subscription_key"), os.getenv("tts_region"))
       print(f"Audio file generated.")
+      print(f"Directory: {os.path.dirname(audio_filename)}, Filename: {os.path.basename(audio_filename)}")
     
-      return send_from_directory(directory=os.path.dirname(audio_filename), 
-                                   filename=os.path.basename(audio_filename), 
-                                   as_attachment=True,
-                                   mimetype='audio/wav')
+      audio_url = f"/audio/{os.path.basename(audio_filename)}"
+      return jsonify({"audio_url": audio_url})
   
   except Exception as e: 
+    print(f"Caught exception: {e}")  
     return make_response(jsonify({"error": str(e)}), 400)
 
 @app.route('/')
