17f8b244c4f3b5921cccd86816378079ce7ef957
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-11-27 22:00:50 -0800

    new camera functionality

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,28 @@
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar'
-import Camera from 'react-html5-camera-photo'
+import { useRef, useState } from 'react';
+import {Camera} from "react-camera-pro";
+//@ts-ignore
+
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const camera = useRef(null);
+  const [image, setImage] = useState(null);
+  
   return (
     <main className="flex-col">
       <h1>
         home page
       </h1>
       <div className="flex justify-center">
-        <Camera/>
+        <Camera ref={camera} errorMessages={{
+          noCameraAccessible: 'no camera accessible',
+          permissionDenied: 'permission denied',
+          switchCamera: 'switch cameras',
+          canvas: 'canvas',
+        }} />
       </div>
     </main>
   )
