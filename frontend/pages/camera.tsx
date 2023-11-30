090d1b7ccb1781b1de11d9ab74261b8f8ac5b8d7
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-11-30 15:31:48 -0800

    added using image tab

--- frontend/pages/camera.tsx
+++ frontend/pages/camera.tsx
@@ -6,8 +6,8 @@ export default function MainCamera() {
   const [image, setImage] = useState(null);
   
   return (
-  <div className="flex flex-col lg:justify-between items-center h-[50rem]">
-    <div className="flex self-center h-[60%] w-[80%] m-4">
+  <div className="flex flex-col lg:justify-around items-center h-[50rem]">
+    <div className="flex self-center h-[60%] w-[80%] m-4 lg:h-[30%] lg:w-[40%]">
       <Camera 
         ref={camera} 
         facingMode='environment'
@@ -22,7 +22,7 @@ export default function MainCamera() {
         } 
       />
     </div>
-    <div className="flex flex-row">
+    <div className="flex flex-row mb-8">
       <button
         className="w-14 h-4 bg-white text-black m-4 text-sm	"
         onClick={() => {
@@ -41,8 +41,20 @@ export default function MainCamera() {
         }
       }>
         Take photo
-      </button>
+      </button>      
     </div>
+    {image && 
+      <div>
+        <h1> 
+          Using Photo:
+        </h1>
+        <img
+          width={200}
+          height={200}
+          src={image}
+        />
+      </div>
+    }
   </div>
   )
 };
